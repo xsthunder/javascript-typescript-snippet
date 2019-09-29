a10a8368f9ddf0a48d2064bc998538dc5dc0cfd6
xsthunder <xsthunder@outlook.com>
2019-09-29 13:53:14 +0800

    Update postman-set-env-var.js

--- postman-set-env-var.js
+++ postman-set-env-var.js
@@ -1,3 +1,6 @@
+/**
+  * use in Tests, executed after response ended
+  */
 console.log('responseBody', responseBody, typeof responseBody) // string
 body = JSON.parse(responseBody)
 pm.environment.set("patient_id", body.data.patient_id);
