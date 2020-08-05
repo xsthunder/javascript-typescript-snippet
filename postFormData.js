0a5dc7e1cc979d08aef7670e13ebad246f208a4b
xsthunder <xsthunder@outlook.com>
2020-08-05 21:59:02 +0800

    Update postFormData.js

--- postFormData.js
+++ postFormData.js
@@ -3,9 +3,10 @@
   * 转成axois用form-data格式，需要注意的是即使再network中request playload是formData，也不一定用这个，需要copy as fetch看看
   * @param string url 
   * @param {*} obj 一层obj,不可嵌套obj，如果是多层，需要对第二层作JSON.stringify
-  * @param instance axios instance
   */
-const postFormData = (url, obj, instance)=>{
+const postFormData = (url, obj)=>{
+    // 如果在node中
+    // var FormData = require('form-data');
     const bodyFormData = new FormData(); 
     Object.entries(obj).map(([k,v])=>bodyFormData.set(k,v)); 
     return axios(url, { 
