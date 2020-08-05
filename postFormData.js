7acace84337347a21ec0fb1116db028ae5921386
xsthunder <xsthunder@outlook.com>
2020-08-05 22:05:23 +0800

    Update postFormData.js

--- postFormData.js
+++ postFormData.js
@@ -1,3 +1,10 @@
+/**
+ * formData
+ * perfers append over set
+ ＊can also append files
+ * [FormData.set() - Web APIs | MDN](https://developer.mozilla.org/en-US/docs/Web/API/FormData/set)
+ * [FormData.append() - Web APIs | MDN](https://developer.mozilla.org/en-US/docs/Web/API/FormData/append)
+ */
 
  /**
   * 转成axois用form-data格式，需要注意的是即使再network中request playload是formData，也不一定用这个，需要copy as fetch看看
@@ -8,7 +15,7 @@ const postFormData = (url, obj)=>{
     // 如果在node中
     // var FormData = require('form-data');
     const bodyFormData = new FormData(); 
-    Object.entries(obj).map(([k,v])=>bodyFormData.set(k,v)); 
+    Object.entries(obj).map(([k,v])=>bodyFormData.append(k,v)); 
     return axios(url, { 
         method: 'post', 
         headers: {'Content-Type': 'multipart/form-data' },
@@ -28,7 +35,7 @@ export default Myservice{
  constructor(private http: HttpClient) { }
  private postFormData(url, obj: Object): Observable<IData> {
    const formData = new FormData();
-   Object.entries(obj).map(([k, v]) => formData.set(k, v));
+   Object.entries(obj).map(([k, v]) => formData.append(k, v));
    return this.http.post(url, formData).pipe(
      map((ob: Observable<ICommonAPI>) => {
        const code = ob['code']
