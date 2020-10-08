735d804c51de1dd4c187af1188c7d33efd76f063
xsthunder <xsthunder@outlook.com>
2020-10-08 20:04:31 +0800

    Create declare-var.ts

---
+++ declare-var.ts
@@ -0,0 +1,7 @@
+// for emcc
+// add `<script async type="text/javascript" src="%PUBLIC_URL%/hash.js"></script>` to `public/index.html`
+
+export declare var Module:any
+// declare function _malloc(size: number): number
+export declare function stringToUTF8(input: string, buf: number, size: number): void
+export declare function UTF8ToString(buf: number, size: number): string
